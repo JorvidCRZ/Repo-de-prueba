0c8ca253d5b5484f9bb5c784183803189f4e1d42
Gerson Osmar Machuca Abanto <U23252429@gmail.com>
2026-04-21 14:49:49 -0500

    agregando funcionalidad a boton cambiar color

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 console.log("Hola desde la funcion de Ana")
 console.log("hola desde la funcion de Gerson");
+
+
+
+let cards = document.querySelectorAll('.card'); 
+let btn = document.querySelector('button');
+
+btn.addEventListener("click", e => {
+    
+    cards.forEach(card => {
+        card.style.background = 'blue';
+    });
+});
